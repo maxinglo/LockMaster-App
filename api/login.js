60f44e295f0f660bdf40ae1c0b42d7ceeabc5efb
maxinglo <2965346066@qq.com>
2024-10-24 17:10:24 +0800

    初次提交

---
+++ api/login.js
@@ -0,0 +1,77 @@
+import request from '@/utils/request'
+
+// 登录方法
+export function login(username, password, code, uuid) {
+  const data = {
+    username,
+    password,
+    code,
+    uuid
+  }
+  console.log("调用login")
+  return request({
+    'url': '/login',
+    headers: {
+      isToken: false
+    },
+    'method': 'post',
+    'data': data
+  })
+}
+// 微信登录方法
+export function wxLogin(appid, code) {
+	console.log("调用wxLogin")
+	const data = {
+		appid,
+		code
+	}
+  return request({
+    'url': `/wxLogin`,
+    headers: {
+      isToken: false
+    },
+    'method': 'post',
+	'data': data
+  });
+}
+
+
+// 注册方法
+export function register(data) {
+  return request({
+    url: '/register',
+    headers: {
+      isToken: false
+    },
+    method: 'post',
+    data: data
+  })
+}
+
+// 获取用户详细信息
+export function getInfo() {
+  return request({
+    'url': '/getInfo',
+    'method': 'get'
+  })
+}
+
+// 退出方法
+export function logout() {
+  return request({
+    'url': '/logout',
+    'method': 'post'
+  })
+}
+
+// 获取验证码
+export function getCodeImg() {
+  return request({
+    'url': '/captchaImage',
+    headers: {
+      isToken: false
+    },
+    method: 'get',
+    timeout: 20000
+  })
+}
---
+++ config.js
@@ -0,0 +1,28 @@
+// 应用全局配置
+module.exports = {
+	// wx67ba303f453b1df0
+	// 8c5ede713532ef7f4f7d40044a5248ae
+  //  baseUrl: 'https://vue.ruoyi.vip/prod-api',
+  baseUrl: 'http://localhost:8080',
+  // 应用信息
+  appInfo: {
+    // 应用名称
+    name: "ruoyi-app",
+    // 应用版本
+    version: "1.1.0",
+    // 应用logo
+    logo: "/static/logo.png",
+    // 官方网站
+    site_url: "http://ruoyi.vip",
+    // 政策协议
+    agreements: [{
+        title: "隐私政策",
+        url: "https://ruoyi.vip/protocol.html"
+      },
+      {
+        title: "用户服务协议",
+        url: "https://ruoyi.vip/protocol.html"
+      }
+    ]
+  }
+}
